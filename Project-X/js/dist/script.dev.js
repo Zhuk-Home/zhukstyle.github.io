49ecd29c9845abd8e9f97e7911cf7a64a9bf3e1a
zhukstyle <73714216+zhukstyle@users.noreply.github.com>
2021-01-10 15:01:28 +0200

    beta 015.1

--- Project-X/js/dist/script.dev.js
+++ Project-X/js/dist/script.dev.js
@@ -140,7 +140,7 @@ $(document).ready(function () {
     // values from 0 to 3000, with step 50ms
     easing: 'ease-in-out',
     // default easing for AOS animations
-    once: false,
+    once: true,
     // whether animation should happen only once - while scrolling down
     mirror: false,
     // whether elements should animate out while scrolling past them
